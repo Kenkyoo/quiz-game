5fcc39fa7497336c7951c6fdac5c30677b582b25
Franco <frfantusatti@protonmail.com>
2024-10-14 07:22:24 -0300

    Add a new function to set score

---
+++ src/components/message.js
@@ -0,0 +1,40 @@
+export function showCompletionMessage(currentScore, totalQuestions) {
+    const message = setResult(currentScore);
+    document.querySelector('.swiper').style.display = "none";
+    document.getElementById('message-container').innerHTML = `  
+    <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="images/office.jpg">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">${currentScore} de ${totalQuestions}<i class="material-icons right">close</i></span>
+        <p>${message}</p>
+      </div>
+    </div>
+    `;
+  }
+
+  function setResult(currentScore) {
+    let message;
+    switch (true) {
+        case (currentScore < 3):
+          message = "Siempre puedes volver a intentarlo";
+          break;
+        case (currentScore < 5):
+          message = "No esta tan mal!!";
+          break;
+        case (currentScore < 8):
+          message = "Muy bueno";
+          break;
+        case (currentScore == 10) :
+            message = "Wowww!!!!";
+          break;    
+        default:
+          break;
+      }
+      return message;
+  }
--- src/components/swiper.js
+++ src/components/swiper.js
@@ -6,6 +6,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+import { showCompletionMessage } from './message';
+
 export function initSwiper () {
   const swiperInstance =   new Swiper('.swiper', {
     // Optional parameters
@@ -32,22 +34,4 @@ export function initSwiper () {
     },    
   });
   return swiperInstance;
-}
-
-function showCompletionMessage(currentScore, totalQuestions) {
-  document.getElementById('message-container').innerHTML = `  
-  <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src="images/office.jpg">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">${currentScore} de ${totalQuestions}<i class="material-icons right">close</i></span>
-      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-    </div>
-  </div>
-  `;
 }

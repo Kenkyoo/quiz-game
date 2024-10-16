4a9db3726171bb7a2e1319fab972481439e92f7f
Franco <frfantusatti@protonmail.com>
2024-10-16 06:35:55 -0300

    Trabajo terminado

--- src/components/cards.js
+++ src/components/cards.js
@@ -3,14 +3,15 @@ import 'materialize-css'
 export function cards(title, category, difficulty, answers, idx) {
 return `  
     <div class="swiper-slide z-depth-5">
-      <div class="card blue-grey darken-1">
+      <div class="card">
         <div class="card-content white-text">
           <span class="card-title">${title}</span>
-          <p>${category}</p>
-          <p>${difficulty}</p>
+          <p>Category: ${category}</p>
+          <p>Difficulty: ${difficulty}</p>
           <p>Question number: ${idx}</p>
         </div>
         <div class="card-action">
+          Options: 
           ${answers.map(answer => `<a data-answer="${answer}" onclick="getAnswers(this)" href="#">${answer}</a>`).join('')}
         </div>
       </div>
--- src/components/message.js
+++ src/components/message.js
@@ -2,18 +2,30 @@ export function showCompletionMessage(currentScore, totalQuestions) {
     const message = setResult(currentScore);
     document.querySelector('.swiper').style.display = "none";
     document.getElementById('message-container').innerHTML = `  
-    <div class="card animate__animated animate__tada">
+    <div class="card card-message animate__animated animate__tada">
       <div class="card-image waves-effect waves-block waves-light">
-        <img class="activator" src="images/office.jpg">
+        <img class="activator" src="https://images.unsplash.com/photo-1576610981602-438b2860eb77?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
       </div>
       <div class="card-content">
-        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-        <p><a href="#">This is a link</a></p>
+        <h2 class="white-text">Congrats!! Do you see your score??</h2>
+        <span class="card-title activator white-text text-darken-4">See my score<i class="material-icons right">more_vert</i></span>
       </div>
       <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">${currentScore} de ${totalQuestions}<i class="material-icons right">close</i></span>
-        <p>${message}</p>
-      </div>
+        <div class="section no-pad-bot">
+          <div class="container>
+            <div class="row center">
+              <h1 class="header center col s12">${currentScore} scores from ${totalQuestions} questions</h1>
+            </div>  
+            <div class="row center">
+              <h5 class="header col s12">${message}</h5>
+            </div>
+            <div class="row center">
+              <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Do you try again?</a>
+            </div>
+            <br><br>
+          </div>
+        </div>
+      </div>  
     </div>
     `;
   }
